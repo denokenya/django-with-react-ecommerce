e719ebe2088d92dfa1c828f9632c3bf4ae2edf03
mbuguap <petrmbugua@gmail.com>
2021-05-19 16:34:02 +0300

    created django backend

---
+++ backend/base/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from .products import products
+
+
+def getRoutes(request):
+
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload',
+
+        '/api/products/<id>/reviews',
+
+        '/api/products/top',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/update/<id>/',
+    ]
+    return JsonResponse(routes, safe=False)
+
+
+def getProducts(request):
+    return JsonResponse(products, safe=False)
+ 
