2b7ae2da6418b057159a1e1e6f9f3bf525aa9e60
mbuguap <petrmbugua@gmail.com>
2021-05-20 13:56:53 +0300

    user register & login

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,20 +2,48 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
+import {
+  userLogin,
+  userRegister,
+  userList,
+  userDelete,
+  userDetails,
+  userUpdate,
+  userUpdateProfile,
+} from './reducers/user';
 import { productList, productDetails } from './reducers/product';
 import { cart } from './reducers/cart';
 
 const reducer = combineReducers({
+  // Products
   productList,
   productDetails,
+
+  // Cart
   cart,
+
+  // Users
+  userLogin,
+  userRegister,
+  userList,
+  userDelete,
+  userDetails,
+  userUpdate,
+  userUpdateProfile,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
-const initialState = { cart: { cartItems: cartItemsFromStorage } };
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const middleware = [thunk];
 
